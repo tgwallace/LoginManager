7d98e58524c6523ffac0fa862988f450f17837aa
BuildTools <unconfigured@null.spigotmc.org>
2024-01-31 15:24:22 -0500

    Added multi-line support for login messages.

--- src/main/java/hardbuckaroo/loginmanager/Listeners.java
+++ src/main/java/hardbuckaroo/loginmanager/Listeners.java
@@ -7,9 +7,6 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
 
-import java.io.File;
-import java.io.IOException;
-
 public class Listeners implements Listener {
     private final LoginManager plugin;
     public Listeners(LoginManager plugin){
@@ -19,11 +16,17 @@ public class Listeners implements Listener {
     @EventHandler
     public void onLogin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
-        String firstMessage = plugin.getConfig().getString("FirstLoginMessage");
-        String message = plugin.getConfig().getString("EveryLoginMessage");
         if(!plugin.getPlayerData().contains(player.getUniqueId().toString())) {
-            if(firstMessage != null && !firstMessage.isEmpty()) player.sendRawMessage(firstMessage);
-            else if(message != null && !message.isEmpty()) player.sendRawMessage(message);
+            if(plugin.getConfig().contains("FirstLoginMessage")) {
+                for (String line : plugin.getConfig().getStringList("FirstLoginMessage")) {
+                    player.sendRawMessage(line);
+                }
+            }
+            else if(plugin.getConfig().contains("EveryLoginMessage")) {
+                for (String line : plugin.getConfig().getStringList("EveryLoginMessage")) {
+                    player.sendRawMessage(line);
+                }
+            }
             if(plugin.getConfig().contains("FirstLoginItems")) {
                 for(String line : plugin.getConfig().getStringList("FirstLoginItems")) {
                     String[] items = line.split(",");
@@ -34,7 +37,11 @@ public class Listeners implements Listener {
             plugin.getPlayerData().set(player.getUniqueId().toString(),player.getName());
             plugin.savePlayerData();
         } else {
-            if(message != null && !message.isEmpty()) player.sendRawMessage(message);
+            if(plugin.getConfig().contains("EveryLoginMessage")) {
+                for (String line : plugin.getConfig().getStringList("EveryLoginMessage")) {
+                    player.sendRawMessage(line);
+                }
+            }
         }
     }
 }
